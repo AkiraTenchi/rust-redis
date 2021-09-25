718e87bda00b7079a1a8f435649446599eb982fa
AkiraTenchi <34791734+AkiraTenchi@users.noreply.github.com>
2021-09-25 22:09:09 +0200

    implemented multi client support

--- examples/first-steps.rs
+++ examples/first-steps.rs
@@ -1,7 +1,7 @@
 use mini_redis::{client, Result};
 
 #[tokio::main]
-pub async fn main() -> Result<()>{
+pub async fn main() -> Result<()> {
     // Open connection to Redis server
     let mut client = client::connect("127.0.0.1:6379").await?;
 
@@ -11,7 +11,10 @@ pub async fn main() -> Result<()>{
     // Get the value for key 'hello'
     let result = client.get("hello").await?;
 
-    println!("value for key 'hello' has been retrieved from server; result={:?}", result);
+    println!(
+        "value for key 'hello' has been retrieved from server; result={:?}",
+        result
+    );
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
-use tokio::{net::{TcpListener, TcpStream}};
+use bytes::Bytes;
 use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use tokio::net::{TcpListener, TcpStream};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     //Bind TcpListener to address
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    loop{
+    //data storage
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
+    loop {
+        let db = db.clone();
+
         //get socket from listener and ignore the IP and port of the connection
         let (socket, _) = listener.accept().await.unwrap();
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    //data storage
-    let mut db = HashMap::new();
 
     //Connection enables reading/writing redis frames
     let mut connection = Connection::new(socket);
@@ -28,13 +34,16 @@ async fn process(socket: TcpStream) {
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
+                //lock db to get access
+                let mut db = db.lock().unwrap();
                 //Store value as 'Vec<u8>'
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
