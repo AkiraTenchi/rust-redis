048502b8103cea857e062ee9e179d17926bd3399
AkiraTenchi <34791734+AkiraTenchi@users.noreply.github.com>
2021-09-25 15:42:15 +0200

    implementet basic functionality and added one example

---
+++ examples/first-steps.rs
@@ -0,0 +1,17 @@
+use mini_redis::{client, Result};
+
+#[tokio::main]
+pub async fn main() -> Result<()>{
+    // Open connection to Redis server
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    // Set 'hello' key to value 'world'
+    client.set("hello", "world".into()).await?;
+
+    // Get the value for key 'hello'
+    let result = client.get("hello").await?;
+
+    println!("value for key 'hello' has been retrieved from server; result={:?}", result);
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{net::{TcpListener, TcpStream}};
+use mini_redis::{Connection, Frame};
+
+#[tokio::main]
+async fn main() {
+    //Bind TcpListener to address
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop{
+        //get socket from listener and ignore the IP and port of the connection
+        let (socket, _) = listener.accept().await.unwrap();
+        process(socket).await;
+    }
+}
+
+async fn process(socket: TcpStream) {
+    //Connection enables reading/writing redis frames
+    let mut connection = Connection::new(socket);
+
+    if let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("GOT: {:?}", frame);
+
+        //Respond with error since not implemented yet
+        let response = Frame::Error("unimplemented".to_string());
+        connection.write_frame(&response).await.unwrap();
+    }
 }
