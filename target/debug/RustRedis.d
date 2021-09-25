e5bae228924c33e66215d3542b535344219c6c03
AkiraTenchi <34791734+AkiraTenchi@users.noreply.github.com>
2021-09-25 15:14:48 +0200

    inital commit

---
+++ target/debug/RustRedis.d
@@ -0,0 +1,1 @@
+/home/akira/Documents/Rust/rust_redis/target/debug/RustRedis: /home/akira/Documents/Rust/rust_redis/src/main.rs
---
+++ target/debug/deps/RustRedis-53907e5aeb8ded03.d
@@ -0,0 +1,5 @@
+/home/akira/Documents/Rust/RustRedis/target/debug/deps/RustRedis-53907e5aeb8ded03: src/main.rs
+
+/home/akira/Documents/Rust/RustRedis/target/debug/deps/RustRedis-53907e5aeb8ded03.d: src/main.rs
+
+src/main.rs:
